d1411fc3dd85f8a76126e3ed9728c2e540f831e3
Falkzera <falcovisk@gmail.com>
2024-07-09 22:55:24 -0300

    Task List Easy

---
+++ Task_List_Easy.py
@@ -0,0 +1,41 @@
+import os
+
+lista = []
+
+while True:
+    
+    pergunta = input('Escolha uma opção: \n' '[S]air [A]dicionar [R]emover [L]istar : ').strip().lower()
+
+    if pergunta.startswith('a'):
+        adicionar = input('Valor: ')
+        lista.append(adicionar)
+        os.system('cls')
+        print(f'{adicionar} foi adicionado.')
+
+    elif pergunta.startswith('r'):
+        try:
+            for indice, nome in enumerate(lista):             
+                print(indice, nome) 
+            remover = int(input('Digite o indice que deseja remover?: '))      
+            removido = lista.pop(remover)
+            os.system('cls')
+            print(f'{removido} foi removido')
+        except: 
+            os.system('cls')
+            print('Valor digitado não contém na lista.')
+
+    elif pergunta.startswith('l'):
+        os.system('cls')
+        for indice, nome in enumerate(lista):             
+             print(indice, nome) 
+    
+    elif pergunta.startswith('s'):
+        os.system('cls')
+        sair = input('Tem certeza que deseja sair? s/n ').strip().lower()
+        if sair.startswith('s'):
+            break
+        else:
+            continue
+    else:
+        os.system('cls')
+        print('Opção inválida.')  
